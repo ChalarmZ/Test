b0222b5109bddfd8e5339b11a9e2740388f0776e
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-17 06:42:08 +0700

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -11,7 +11,7 @@ for _, v2 in next, Foods do
     for _, v in next, workspace.PlayerPens:GetChildren() do
         if v:GetAttribute("Owner") == LocalPlayer.Name then
             for _,v1 in next, v.Pets:GetChildren() do 
-                ReplicatedStorage.Packages._Index["sleitnick_knit@1.7.0"].knit.Services.FoodService.RF.FeedPet:InvokeServer(v2, v1.Name, 0/0)
+                ReplicatedStorage.SharedModules.Packet.RemoteEvent(v2, v1.Name, 0/0)
             end 
         end
     end
