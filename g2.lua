14468f2241c9ebb899b2bc93c8611b1c0cf15f8a
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-17 06:59:48 +0700

    Create g2.lua

---
+++ g2.lua
@@ -0,0 +1,16 @@
+local Players = game:GetService("Players")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local Foods = {
+    "Bamboo"
+}
+
+local LocalPlayer = Players.LocalPlayer
+
+    for _, v in next, Foods do
+        if v:GetAttribute("Owner") == LocalPlayer.Name then
+            for _,v1 in next, v.Pets:GetChildren() do 
+                ReplicatedStorage.SharedModules.Packet.RemoteEvent(v1.name, 9999)
+            end 
+        end
+    end
