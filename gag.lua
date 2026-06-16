d00475f25742a19f32951f5a19758fdb5b5c003e
ChalarmZ <keemzanznr4747@gmail.com>
2026-06-17 06:56:47 +0700

    Update gag.lua

--- gag.lua
+++ gag.lua
@@ -6,6 +6,11 @@ local Foods = {
 }
 
 local LocalPlayer = Players.LocalPlayer
-    for _,v1 in next, Foods do 
-        ReplicatedStorage.SharedModules.Packet.RemoteEvent(v1, 0/0)
-    end 
+
+    for _, v in next, workspace.PlayerPens:GetChildren() do
+        if v:GetAttribute("Owner") == LocalPlayer.Name then
+            for _,v1 in next, v.Pets:GetChildren() do 
+                ReplicatedStorage.SharedModules.Packet.RemoteEvent(v1.name, 9999)
+            end 
+        end
+    end
